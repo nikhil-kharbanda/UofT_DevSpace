117d547ac36780e91ddd286c79e93d95026d5278
Wickette <stephany.clark2@gmail.com>
2021-11-25 16:08:13 -0500

    login and signup backend working

--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -8,6 +8,8 @@ import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../../utils/mutations";
 import Auth from '../../utils/auth';
 
+//test
+
 const Signup = (props) => {
   const [show, setShow] = useState(false);
   const [pic, setPic] = useState();
