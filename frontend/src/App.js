abab1d4a51b94a337a98a02002ea1a35dec8edc2
Wickette <stephany.clark2@gmail.com>
2021-11-25 16:06:37 -0500

    login/signup backend working again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,47 @@
-import { Route } from 'react-router-dom';
-import './App.css';
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
 import ChatPage from './Pages/ChatPage';
 import Homepage from './Pages/Homepage';
+import LoginSignup from './Pages/Login-Signup';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <div className="App">
-      <Route path='/' component={Homepage} exact/>
-      <Route path='/chats' component={ChatPage}/>
+      <ApolloProvider client={client}>
+        <Router>
+          <Route exact path='/' component={Homepage}/>
+          <Route exact path='/Login' component={LoginSignup}/>
+          <Route exact path='/chats' component={ChatPage}/>
+        </Router>
+      </ApolloProvider>
     </div>
   );
 }
--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,53 +1,11 @@
-
-import React from "react";
-import {
-  Container,
-  Box,
-  Text,
-  Tabs,
-  TabList,
-  Tab,
-  TabPanel,
-  TabPanels,
-} from "@chakra-ui/react";
-import Login from '../components/Authentication/Login'
-import Signup from "../components/Authentication/Signup";
+import React from 'react'
 
 const Homepage = () => {
-  return (
-    <Container maxW="xl" centerContent>
-      <Box
-        d="flex"
-        justifyContent="center"
-        p={3}
-        bg={"white"}
-        w="100%"
-        m="40px 0 15px 0"
-        borderRadius="lg"
-        borderWidth="1px"
-      >
-        <Text fontSize="4xl" fontFamily="Work sans">
-          Chat Room
-        </Text>
-      </Box>
-      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
-        <Tabs isFitted variant="soft-rounded">
-          <TabList mb="1em">
-            <Tab>Login</Tab>
-            <Tab>Sign Up</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <Login />
-            </TabPanel>
-            <TabPanel>
-              <Signup />
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </Box>
-    </Container>
-  );
-};
+    return (
+        <div>
+            <h1>Homepage</h1>
+        </div>
+    )
+}
 
-export default Homepage;
+export default Homepage
