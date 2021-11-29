3c0e918830e78ddeea959a86e4261dab7d978ab8
Nikhil Kharbanda <nikhilkharbanda@cmail.carleton.ca>
2021-11-29 14:46:38 -0500

    push to heroku2

--- backend/server.js
+++ backend/server.js
@@ -31,17 +31,17 @@ const server = new ApolloServer({
 //middleware
 app.use(express.json());
 app.use(cors())
-app.use(express.static("client/build"))
+app.use(express.static("frontend/build"))
 
 server.applyMiddleware({ app });
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
   }
   
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
   });
   
 //api routes
