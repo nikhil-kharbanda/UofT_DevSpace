563bd86ac46313e858169f78f26485fe1fc6fb03
Wickette <stephany.clark2@gmail.com>
2021-11-28 19:44:41 -0500

    merging Mike's frontend into backend server and running with concurrently

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import store from './app/store';
+import { Provider } from 'react-redux';
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter } from "react-router-dom"
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Provider store={store}>
       <ChakraProvider>
         <App />
       </ChakraProvider>
-    </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+serviceWorker.unregister();
+
